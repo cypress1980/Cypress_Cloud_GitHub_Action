e36d20d9c114dcc16b7cef7d92d07506f3421bae
Kailash Chander Pathak <you@example.com>
2024-01-23 20:40:51 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "u2jf31",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
