0501d222c6b22e685b2f1d4eaad6f0c5e4f137ec
Kailash Chander Pathak <you@example.com>
2024-01-28 20:08:54 +0530

    update test case to pass it again

--- cypress/e2e/cypressCloudTC-001.cy.js
+++ cypress/e2e/cypressCloudTC-001.cy.js
@@ -4,7 +4,7 @@ describe('Cypress Cloud Demo', () => {
      cy.get('[data-test="username"]').clear('standard_user');
      cy.get('[data-test="username"]').type('standard_user');
      cy.get('[data-test="password"]').clear('secret_sauce');
-     cy.get('[data-test="password"]').type('secret_sauce1');
+     cy.get('[data-test="password"]').type('secret_sauce');
      cy.get('[data-test="login-button"]').click();
      cy.get('#react-burger-menu-btn').click();
      cy.get('#logout_sidebar_link').click();
