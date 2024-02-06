0376c0dfbfeadda62ccc8db2a0c9c1bd6081c4e0
Kailash Chander Pathak <you@example.com>
2024-02-06 15:39:38 +0530

    Added new tc

--- cypress/e2e/cypressCloudTC-004.cy.js
+++ cypress/e2e/cypressCloudTC-004.cy.js
@@ -3,9 +3,6 @@ describe("UI QAAutomationLabs.com", { testIsolation: false }, () => {
     it("Open URL", () => {
       cy.visit("https://qaautomationlabs.com/");
     });
-    it("Click on Read More ", () => {
-      cy.get(".staticslider-button").click();
-    });
     it("Verify Particular Blog ", () => {
       cy.contains(
         "Running End-to-End Cypress Test cases in a Google Cloud Pipeline"
